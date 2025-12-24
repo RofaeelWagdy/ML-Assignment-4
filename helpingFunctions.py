fix: update covariance calculations and parameter estimation in GMM class

helpingFunctions.py
@@ -124,13 +124,29 @@ class GMM:
             nk = np.sum(resp, axis=0)
             self.weights = nk / n
             
+            # Update means
             for j in range(self.k):
                 self.means[j] = np.sum(resp[:, [j]] * X, axis=0) / nk[j]
-                diff = X - self.means[j]
-                if self.cov_type == 'full':
-                    self.covs[j] = np.dot((resp[:, [j]] * diff).T, diff) / nk[j]
-                elif self.cov_type == 'diagonal':
-                    self.covs[j] = np.diag(np.sum(resp[:, [j]] * (diff**2), axis=0) / nk[j])
+
+            # Update covariances
+            if self.cov_type == 'tied':
+                tied_cov = np.zeros((d, d))
+                for j in range(self.k):
+                    diff = X - self.means[j]
+                    tied_cov += np.dot((resp[:, [j]] * diff).T, diff)
+                tied_cov /= n
+                self.covs = [tied_cov for _ in range(self.k)]
+            
+            else:
+                for j in range(self.k):
+                    diff = X - self.means[j]
+                    if self.cov_type == 'full':
+                        self.covs[j] = np.dot((resp[:, [j]] * diff).T, diff) / nk[j]
+                    elif self.cov_type == 'diagonal':
+                        self.covs[j] = np.diag(np.sum(resp[:, [j]] * (diff**2), axis=0) / nk[j])
+                    elif self.cov_type == 'spherical':
+                        avg_var = np.sum(resp[:, [j]] * (diff**2)) / (nk[j] * d)
+                        self.covs[j] = np.eye(d) * avg_var
 
             curr_ll = np.sum(np.log(sum_probs))
             self.ll_history.append(curr_ll)
@@ -138,6 +154,15 @@ class GMM:
                 break
         
         # Calculate BIC/AIC (Parameters estimation depends on cov_type)
-        p = self.k * (d + (d*(d+1)/2 if self.cov_type=='full' else d)) + (self.k - 1)
+        if self.cov_type == 'full':
+            cov_params = self.k * d * (d + 1) / 2
+        elif self.cov_type == 'tied':
+            cov_params = d * (d + 1) / 2
+        elif self.cov_type == 'diagonal':
+            cov_params = self.k * d
+        elif self.cov_type == 'spherical':
+            cov_params = self.k
+            
+        p = (self.k * d) + cov_params + (self.k - 1)
         self.bic = -2 * self.ll_history[-1] + p * np.log(n)
         self.aic = -2 * self.ll_history[-1] + 2 * p