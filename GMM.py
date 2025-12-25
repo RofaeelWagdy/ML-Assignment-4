5683ef133508697959ebc7f9a3272fd0ee4f1c99
RofaeelWagdy <rofaeelwagdy@gmail.com>
2025-12-25 11:41:09 +0200

    refactor: standardize variable names and improve code clarity in GMM class

--- GMM.py
+++ GMM.py
@@ -2,80 +2,82 @@ import numpy as np
 
 class GMM:
     def __init__(self, n_components, cov_type='full', max_iter=100, tol=1e-4):
-        self.k = n_components
+        self.n_clusters = n_components
         self.cov_type = cov_type
         self.max_iter = max_iter
-        self.tol = tol
+        self.tolerance = tol
 
     def _gaussian_pdf(self, X, mean, cov):
-        d = X.shape[1]
-        cov += 1e-6 * np.eye(d)
+        dimension_count = X.shape[1]
+        cov += 1e-6 * np.eye(dimension_count)
         det = np.linalg.det(cov)
         inv = np.linalg.inv(cov)
         diff = X - mean
         exponent = -0.5 * np.sum(np.dot(diff, inv) * diff, axis=1)
-        return (1.0 / np.sqrt(((2 * np.pi)**d) * det)) * np.exp(exponent)
+        return (1.0 / np.sqrt(((2 * np.pi)**dimension_count) * det)) * np.exp(exponent)
 
     def fit(self, X):
-        n, d = X.shape
-        self.weights = np.ones(self.k) / self.k
-        self.means = X[np.random.choice(n, self.k, replace=False)]
-        self.covs = [np.eye(d) for _ in range(self.k)]
-        self.ll_history = []
+        sample_count, dimension_count = X.shape
+        self.weights = np.ones(self.n_clusters) / self.n_clusters
+        self.means = X[np.random.choice(sample_count, self.n_clusters, replace=False)]
+        self.covs = [np.eye(dimension_count) for _ in range(self.n_clusters)]
+        self.log_likelihood_history = []
 
         for _ in range(self.max_iter):
             # E-step
-            probs = np.zeros((n, self.k))
-            for j in range(self.k):
+            probs = np.zeros((sample_count, self.n_clusters))
+            for j in range(self.n_clusters):
                 probs[:, j] = self.weights[j] * self._gaussian_pdf(X, self.means[j], self.covs[j])
             
+            # normalization of probalities (do that the sum = 1)
             sum_probs = np.sum(probs, axis=1, keepdims=True) + 1e-15
             resp = probs / sum_probs
             
+
             # M-step
-            nk = np.sum(resp, axis=0)
-            self.weights = nk / n
+            cluster_sample_count = np.sum(resp, axis=0)
+            self.weights = cluster_sample_count / sample_count
             
             # Update means
-            for j in range(self.k):
-                self.means[j] = np.sum(resp[:, [j]] * X, axis=0) / (nk[j] + 1e-9)
+            for j in range(self.n_clusters):
+                self.means[j] = np.sum(resp[:, [j]] * X, axis=0) / (cluster_sample_count[j] + 1e-9)
 
             # Update covariances
             if self.cov_type == 'tied':
-                tied_cov = np.zeros((d, d))
-                for j in range(self.k):
+                tied_cov = np.zeros((dimension_count, dimension_count))
+                for j in range(self.n_clusters):
                     diff = X - self.means[j]
                     tied_cov += np.dot((resp[:, [j]] * diff).T, diff)
-                tied_cov /= n
-                self.covs = [tied_cov for _ in range(self.k)]
+                tied_cov /= sample_count
+                self.covs = [tied_cov for _ in range(self.n_clusters)]
             
             else:
-                for j in range(self.k):
+                for j in range(self.n_clusters):
                     diff = X - self.means[j]
                     if self.cov_type == 'full':
-                        self.covs[j] = np.dot((resp[:, [j]] * diff).T, diff) / (nk[j] + 1e-9)
+                        self.covs[j] = np.dot((resp[:, [j]] * diff).T, diff) / (cluster_sample_count[j] + 1e-9)
                     elif self.cov_type == 'diag':
-                        self.covs[j] = np.diag(np.sum(resp[:, [j]] * (diff**2), axis=0) / (nk[j] + 1e-9))
+                        self.covs[j] = np.diag(np.sum(resp[:, [j]] * (diff**2), axis=0) / (cluster_sample_count[j] + 1e-9))
                     elif self.cov_type == 'spherical':
-                        avg_var = np.sum(resp[:, [j]] * (diff**2)) / (nk[j] + 1e-9)
-                        self.covs[j] = np.eye(d) * avg_var
+                        avg_var = np.sum(resp[:, [j]] * (diff**2)) / (cluster_sample_count[j] + 1e-9)
+                        self.covs[j] = np.eye(dimension_count) * avg_var
 
-            curr_ll = np.sum(np.log(sum_probs))
-            self.ll_history.append(curr_ll)
-            if len(self.ll_history) > 1 and abs(curr_ll - self.ll_history[-2]) < self.tol:
+            current_log_likelihood = np.sum(np.log(sum_probs))
+            self.log_likelihood_history.append(current_log_likelihood)
+            if len(self.log_likelihood_history) > 1 and abs(current_log_likelihood - self.log_likelihood_history[-2]) < self.tolerance:
                 break
         
         # Calculate BIC/AIC (Parameters estimation depends on cov_type)
         if self.cov_type == 'full':
-            cov_params = self.k * d * (d + 1) / 2
+            cov_params = self.n_clusters * dimension_count * (dimension_count + 1) / 2
         elif self.cov_type == 'tied':
-            cov_params = d * (d + 1) / 2
+            cov_params = dimension_count * (dimension_count + 1) / 2
         elif self.cov_type == 'diag':
-            cov_params = self.k * d
+            cov_params = self.n_clusters * dimension_count
         elif self.cov_type == 'spherical':
-            cov_params = self.k
+            cov_params = self.n_clusters
             
-        p = (self.k * d) + cov_params + (self.k - 1)
-        self.bic = -2 * self.ll_history[-1] + p * np.log(n)
-        self.aic = -2 * self.ll_history[-1] + 2 * p
-        return self.ll_history
+        parameter_count = (self.n_clusters * dimension_count) + cov_params + (self.n_clusters - 1)
+        self.bic = -2 * self.log_likelihood_history[-1] + parameter_count * np.log(sample_count)
+        self.aic = -2 * self.log_likelihood_history[-1] + 2 * parameter_count
+        return self.log_likelihood_history
